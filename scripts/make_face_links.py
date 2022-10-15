bd735c1eb3727311e492b9bce1a5b3ea4865d54b
laqieer <laqieer@126.com>
2022-10-16 02:25:19 +0800

    Update image links in portrait dataset

--- scripts/make_face_links.py
+++ scripts/make_face_links.py
@@ -7,6 +7,6 @@ links = []
 
 with open('../../FE-Mod-Face-Search/refMap.json') as f:
     for v in json.load(f).values():
-        links += [os.path.join('https://raw.githubusercontent.com/laqieer/FE-Mod-Face-Search/main/datasets/portraits/', os.path.basename(x)) for x in v]
+        links += [os.path.join('https://raw.githubusercontent.com/laqieer/GBA-FE-AI-Art/main/datasets/portraits/', os.path.basename(x)) for x in v]
 
 print(links)
