a29ac121fb3cfe50df457dcdb6f1934042886f7d
laqieer <laqieer@126.com>
2022-10-16 02:59:00 +0800

    Convert images in portrait dataset from RGBA to RGB

--- scripts/process_face_images.py
+++ scripts/process_face_images.py
@@ -9,15 +9,15 @@ with open('../../FE-Mod-Face-Search/refMap.json') as f:
     for v in json.load(f).values():
         for x in v:
             im = Image.open(os.path.join('../../FE-Mod-Face-Search', x))
-            im = im.convert('RGBA')
+            im = im.convert('RGB')
             data = np.array(im)
-            transparent = [0,0,0,0]
+            white = [255,255,255]
             backdrop = list(im.getpixel((0, 0)))
-            if backdrop != transparent:
+            if backdrop != white:
                 # just use the rgb values for comparison
                 rgb = data[:,:,:3]
                 mask = np.all(rgb == backdrop[:3], axis = -1)
-                data[mask] = transparent
-            new_im = Image.new('RGBA', (128, 128), tuple(transparent))
+                data[mask] = white
+            new_im = Image.new('RGB', (128, 128), tuple(white))
             new_im.paste(Image.fromarray(data), (64 - im.width // 2, 128 - im.height))
             new_im.save(os.path.join('../datasets/portraits', os.path.basename(x)))
